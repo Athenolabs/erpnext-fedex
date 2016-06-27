702163b247ea0eb18cc4ac5b306bb640ed007cc7
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-06-27 11:39:29 -0400

    Stuff

--- otherapi/rate.py
+++ otherapi/rate.py
@@ -5,11 +5,15 @@
 #workon fedex-second-api
 
 from fedex.services import FedexService
-import fedex.config
+from fedex.config import FedexConfiguration
 import os.path
 
 file_path = os.path.abspath(__file__)
 dir_name = os.path.dirname(file_path)
 file_name = os.path.join(dir_name, "cred.cfg")
+
+conf = FedexConfiguration(wsdls='beta', file_name=file_name)
+
+
 
 #serv = FedexService
